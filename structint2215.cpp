5282353259fcd6bea6596b2685b2773c2fd5ea68
dotruong1910 <20021331@vnu.edu.vn>
2021-03-23 22:59:53 +0700

    struct bai 5

--- structint2215.cpp
+++ structint2215.cpp
@@ -3,13 +3,13 @@
 #include<vector>
 using namespace std;
 struct Student{
-    int msv;
+    string msv;
     string name;
     double gpa;
 };
 struct Biginteger{
     int sign;
-    string n;
+    string num;
 };
 struct Greeting{
     string greet;
@@ -43,26 +43,83 @@ Fraction reduceFrac( Fraction &n){
     n.denominator = n.denominator/t;
     return n;
 }
-/* Biginteger sum(Biginteger first, Biginteger second){
-} */
+Biginteger sum(Biginteger first, Biginteger second){
+    int len1 =first.num.length();
+    int len2 = second.num.length();
+    string zeronum ="";
+    if(len1 - len2 > 0 || len2 - len1 > 0){
+        for(int  i= 0 ; i<abs(len1 - len2);i++){
+            zeronum+='0';
+        }
+        if(len 1 > len 2){
+            string sum =" "+first.num;
+            char c[len1];
+            for(int i=len1 ; i >= 0;i++){
+                c[i] = '0';
+            }
+            second.num = zeronum+second.num;
+            for(int i = len1-1 ; i>=0;i--){
+                if(first.num[i]+48 +second.num[i]+48+c[i] >= 10){
+                    c[i-1] ='1';
+                    sum[i]=first.num[i]+48 +second.num[i]+48 + c[i] + 48 -'10';
+                }
+                else{
+                    sum[i]=first.num[i]+48 +second.num[i]+48 + c[i] + 48 -'10';
+                }
+
+            }
+        
+        }    
+        else{
+            first.num = zeronum + first.num;
+        } 
+
+
+    }
+
+
+
+
+    string tong;
+} 
 int main(){
     int n; cin >> n;
     Student st;
+    string _delblank;
     char c;
-    double max = -999999.0;
+    int index;
+    double max = 0;
     vector<string> v;
     vector<double> _gpa;
     for(int i = 0; i < n;i++){
-        cin >> st.msv>>c;
+        getline(cin,st.msv,',');
+        cin.ignore(256,' ');
         getline(cin,st.name,',');
         v.push_back(st.name);
+        //cin.ignore(256,' ');
         cin >> st.gpa;
         _gpa.push_back(st.gpa);
 
     }
     for(int i = 0 ; i<n;i++){
-        if(_gpa.at(i) > max) max = i;
+        if(_gpa.at(i) - max > 0){max = _gpa.at(i);
+        
+        index = i;
+        }
+        
     }
-    cout <<v.at(max)<<endl;
+    /* for(int i =0 ; i<n;i++){
+        cout<<_gpa.at(i)<<" ";
+    } 
+    cout<<endl;
+    for(int i =0 ; i<n;i++){
+        cout<<v.at(i)<<" ";
+    }  */
+    cout<<v.at(index);
+    //_delblank = v.at(max);
+    /* for(int i = 0 ; i<n;i++){
+        cout<<_delblank[i];
+    } */
+   // cout <<v.at(max)<<endl;
     return 0;
 }
