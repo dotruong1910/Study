155bdb0b64f210684878dea17ba6bc9fea06777a
dotruong1910 <20021331@vnu.edu.vn>
2021-03-27 10:42:01 +0700

    Big int + and - operator completed

--- structint2215.cpp
+++ structint2215.cpp
@@ -7,13 +7,13 @@
 using namespace std;
 struct BigInteger{
     int sign;
-    string num;
+    string number;
 };
 BigInteger sum(BigInteger first, BigInteger second){
     //lay do dai 2 xau
     BigInteger ketqua;
     string zeronum ="";
-    int len1 =first.num.length(),len2 = second.num.length();
+    int len1 =first.number.length(),len2 = second.number.length();
     // phep +
     if((first.sign == 1 && second.sign ==1) ||(first.sign == 0 && second.sign == 0)){
         if((first.sign == 1 && second.sign ==1)) ketqua.sign = 1; // gan dau +
@@ -29,22 +29,22 @@ BigInteger sum(BigInteger first, BigInteger second){
                 }
                 //so 1 lon hon so 2 phep +  lam swap 2 so
                 if(len1 > len2){
-                    max = first.num;
-                    min = second.num;
+                    max = first.number;
+                    min = second.number;
                     min = zeronum+min;// chhen so 0
                 }
                 else if(len1 <len2){
-                    max=second.num;
-                    min=first.num;
+                    max=second.number;
+                    min=first.number;
                     min = zeronum+min;//chen so 0
                 }
             }
             // TH bang
             else if(len1 == len2){
-                max="0"+first.num;
-                min="0"+second.num;
+                max="0"+first.number;
+                min="0"+second.number;
             }
-                ketqua.num=max;
+                ketqua.number=max;
                 for(int i = max.length()-1 ; i>=0;i--){
                     int check = max[i] - 48 + min[i]-48;
                     int temp  = check %10;
@@ -52,7 +52,7 @@ BigInteger sum(BigInteger first, BigInteger second){
                         temp = 0;
                     }
                     if((check +temp>= 10)){
-                        ketqua.num[i]=temp;
+                        ketqua.number[i]=temp;
                         if(int(min[i-1]-48)+1>=10){
                             min[i-1]='0';
                             min[i-2]=char(min[i-2]+1);    
@@ -63,19 +63,19 @@ BigInteger sum(BigInteger first, BigInteger second){
                     }
                     else{
                         temp = 0;
-                        ketqua.num[i]=int((max[i]-'0') +(min[i]-'0'))  ;
+                        ketqua.number[i]=int((max[i]-'0') +(min[i]-'0'))  ;
                     }
 
                 }
             // co the save time bang cach dung moi ham erase de xoa so 0 o dau
-            if(ketqua.num[0]+'0' == '0'){
-                for(int i =0 ; i<ketqua.num.length()-2;i++){
-                    ketqua.num[i] = ketqua.num[i+1];
+             if(ketqua.number[0]+'0' == '0'){
+                for(int i =0 ; i<ketqua.number.length()-1;i++){
+                    ketqua.number[i] = ketqua.number[i+1];
                 }   
-                ketqua.num.erase(ketqua.num.length()-1);
+                ketqua.number.erase(ketqua.number.length()-1);
             }
-            for(int i =0 ; i<ketqua.num.length();i++){
-                ketqua.num[i]+='0';
+            for(int i =0 ; i<ketqua.number.length();i++){
+                ketqua.number[i]+='0';
             }
         }
         return ketqua;
@@ -84,8 +84,8 @@ BigInteger sum(BigInteger first, BigInteger second){
     else if(first.sign == 1 && second.sign == 0 || first.sign == 0 && second.sign == 1){
         string max,min;
         if(len1 -len2 >0){
-        max = first.num;
-        min = second.num;
+        max = first.number;
+        min = second.number;
         ketqua.sign = first.sign;
         int l = abs(len1 - len2);
     //tao mang so 0;
@@ -95,8 +95,8 @@ BigInteger sum(BigInteger first, BigInteger second){
         min=zeronum+min; 
     }
     else if(len2 -len1>0){
-        max =second.num;
-        min =first.num;
+        max =second.number;
+        min =first.number;
         ketqua.sign = second.sign;
     int l = abs(len1 - len2);
     //tao mang so 0;
@@ -107,16 +107,16 @@ BigInteger sum(BigInteger first, BigInteger second){
     }
     if(len1 == len2){
         for(int i=0 ; i<len1; i++){
-            int check = int((first.num[i]-48))-((second.num[i]-48));
+            int check = int((first.number[i]-48))-((second.number[i]-48));
             if(check >0){
-                max = first.num;
-                min = second.num;
+                max = first.number;
+                min = second.number;
                 ketqua.sign = first.sign;
                 break;
             }
             else if(check <0){
-                max =second.num;
-                min =first.num;
+                max =second.number;
+                min =first.number;
                 ketqua.sign = second.sign;
                 break;
             }
@@ -124,12 +124,12 @@ BigInteger sum(BigInteger first, BigInteger second){
         max="0"+max;
         min="0"+min;
     }
-    ketqua.num =max;
+    ketqua.number =max;
     //cout<<max<<"\n"<<min<<endl;
     for(int i=max.length()-1 ; i>=0;i--){
         int check = int((max[i]-48))-((min[i]-48));
         if(check<0){
-            ketqua.num[i] =(max[i]-48+10)-(min[i]-48);
+            ketqua.number[i] =(max[i]-48+10)-(min[i]-48);
             if(int(min[i-1]-48)+1>=10){
                         min[i-1]='0';
                         min[i-2]=char(min[i-2]+1);    
@@ -138,17 +138,17 @@ BigInteger sum(BigInteger first, BigInteger second){
                         min[i-1]=char(min[i-1]+1);
                     }
         }
-        else ketqua.num[i] = char(int(max[i]-48)-(min[i]-48));
+        else ketqua.number[i] = char((max[i]-48)-(min[i]-48));
     }
-    if(ketqua.num[0]+'0' == '0'){
-                for(int i =0 ; i<ketqua.num.length()-2;i++){
-                    ketqua.num[i] = ketqua.num[i+1];
+    if(ketqua.number[0]+'0' == '0'){
+                for(int i =0 ; i<ketqua.number.length()-1;i++){
+                    ketqua.number[i] = ketqua.number[i+1];
                 }   
-                ketqua.num.erase(ketqua.num.length()-1);
+                ketqua.number.erase(ketqua.number.length()-1);
     }
-    //else if(ketqua.num[0]+'0' == '0' && ketqua.num.length() == 1) ketqua.sign = 0;
-    for(int i =0 ; i<ketqua.num.length();i++){
-            ketqua.num[i]= ketqua.num[i]+'0';
+    //else if(ketqua.number[0]+'0' == '0' && ketqua.number.length() == 1) ketqua.sign = 0;
+    for(int i =0 ; i<ketqua.number.length();i++){
+            ketqua.number[i]= ketqua.number[i]+'0';
         }  
     return ketqua;  
     } 
@@ -159,10 +159,10 @@ BigInteger sum(BigInteger first, BigInteger second){
 
 int main(){
      BigInteger n,n2;
-    cin >> n.sign >> n.num >> n2.sign >> n2.num;
+    cin >> n.sign >> n.number >> n2.sign >> n2.number;
     BigInteger c = sum(n,n2); 
     if(c.sign == 1)
-        cout<<c.num;
+        cout<<c.number;
     else if(c.sign == 0)
-        cout<<"-"+c.num<<"_";
+        cout<<"-"+c.number<<"";
 }
