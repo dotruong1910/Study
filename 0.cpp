25b0c0451133766981d8e6d198887357e54cde5d
dotruong1910 <20021331@vnu.edu.vn>
2021-03-27 10:37:34 +0700

    LTNC Ham nang cao bai 1

--- 0.cpp
+++ 0.cpp
@@ -1,11 +1,40 @@
+//buoc 1 tao ra 1 mang co n so 0 de luu gia gtri nhi phan 
+    //* VD 3 -> 000  , 4->0000
+
 #include<iostream>
+#include<math.h>
 #include<string>
 using namespace std;
-
-int main(){
-    string s ="";
-    for(int i=0;i<0;i++){
-        s+='0';
+void mangnhiphan(int a[],int n){ 
+    for(int i=0;i<n;i++){
+        a[i]=0;
     }
-    cout<<s;
+}
+long duyetmang_trenxuong(int a[],int n){
+    for(int i = n-1;i>=0;i--){
+        if(a[i]==0){
+            a[i]=1;
+            for(int j = i+1;j<n;j++){
+                a[j] = 0;
+            }
+            break;
+        }
+    }
+}
+void inmang(int a[], int n){
+    for(int i = 0;i<n;i++){
+        cout<<a[i];
+    }
+    cout<<endl;
+}
+int main(){
+    int n;  cin >> n;
+    int a[10000];
+    mangnhiphan(a,n);
+    inmang(a,n);
+    for(int i=0;i<pow(2,n)-1;i++){
+        duyetmang_trenxuong(a,n);
+        inmang(a,n);
+        
+    }
 }
